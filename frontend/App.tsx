ec82bf305ae714635f0edd4a2980d06fa4d89ff8
Iva Chekarova <ivachekarova@Ivas-MacBook-Air.local>
2026-04-22 11:56:22 +0200

    feat: initialize Expo frontend structure

---
+++ frontend/App.tsx
@@ -0,0 +1,19 @@
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text } from "react-native";
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>ParkPal</Text>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
